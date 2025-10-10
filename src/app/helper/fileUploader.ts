9970df1af0b072ad982ccba21059698948185d67
raufurislam <raufurislam@gmail.com>
2025-10-10 21:10:36 +0600

    feat(upload): parse form data and prepare upload for cloudinary

--- src/app/helper/fileUploader.ts
+++ src/app/helper/fileUploader.ts
@@ -14,8 +14,11 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-const uploadToCloudinary = async (file: Express.Multer.File) => {};
+const uploadToCloudinary = async (file: Express.Multer.File) => {
+  console.log("file:", file);
+};
 
 export const fileUploader = {
   upload,
+  uploadToCloudinary,
 };
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,31 +1,37 @@
+import { Request } from "express";
 import config from "../../../config";
 import { prisma } from "../../shared/prisma";
-import { createPatientInput } from "./user.interface";
 import bcrypt from "bcryptjs";
+import { fileUploader } from "../../helper/fileUploader";
 
-const createPatient = async (payload: createPatientInput) => {
-  const hashedPassword = await bcrypt.hash(
-    payload.password,
-    Number(config.BCRYPT_SALT_ROUND)
-  );
+const createPatient = async (req: Request) => {
+  if (req.file) {
+    const uploadResult = await fileUploader.uploadToCloudinary(req.file);
+    console.log(uploadResult);
+  }
 
-  const result = prisma.$transaction(async (tnx) => {
-    await tnx.user.create({
-      data: {
-        email: payload.email,
-        password: hashedPassword,
-      },
-    });
+  // const hashedPassword = await bcrypt.hash(
+  //   req.body.password,
+  //   Number(config.BCRYPT_SALT_ROUND)
+  // );
 
-    return await tnx.patient.create({
-      data: {
-        name: payload.name,
-        email: payload.email,
-      },
-    });
-  });
+  // const result = prisma.$transaction(async (tnx) => {
+  //   await tnx.user.create({
+  //     data: {
+  //       email: req.body.email,
+  //       password: hashedPassword,
+  //     },
+  //   });
 
-  return result;
+  //   return await tnx.patient.create({
+  //     data: {
+  //       name: req.body.name,
+  //       email: req.body.email,
+  //     },
+  //   });
+  // });
+
+  // return result;
 };
 
 export const UserService = {
