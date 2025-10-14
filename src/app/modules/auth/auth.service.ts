28e7af1a41226c6818db9e2a4fb70eadf57fc2a4
raufurislam <raufurislam@gmail.com>
2025-10-14 16:47:54 +0600

    feat(auth): implement user login endpoint

---
+++ src/app/modules/auth/auth.service.ts
@@ -0,0 +1,25 @@
+import { UserStatus } from "@prisma/client";
+import { prisma } from "../../shared/prisma";
+import bcrypt from "bcryptjs";
+
+const login = async (payload: { email: string; password: string }) => {
+  const user = await prisma.user.findUniqueOrThrow({
+    where: {
+      email: payload.email,
+      status: UserStatus.ACTIVE,
+    },
+  });
+
+  const isCorrectPassword = await bcrypt.compare(
+    payload.password,
+    user.password
+  );
+
+  if (!isCorrectPassword) {
+    throw new Error("Password is incorrect!");
+  }
+};
+
+export const AuthService = {
+  login,
+};
