ec231185def3c697ab98e7114c1f2e016775858f
raufurislam <raufurislam@gmail.com>
2025-10-20 11:38:41 +0600

    feat(schedule): handle jwt user type and complete schedule creation part-2

--- src/app/modules/doctorSchedule/doctorSchedule.controller.ts
+++ src/app/modules/doctorSchedule/doctorSchedule.controller.ts
@@ -2,11 +2,15 @@ import { Request, Response } from "express";
 import catchAsync from "../../shared/catchAsync";
 import sendResponse from "../../shared/sendResponse";
 import { DoctorScheduleService } from "./doctorSchedule.service";
+import { IJWTPayload } from "../../types/common";
 
 const insertIntoDB = catchAsync(
-  async (req: Request & { user?: any }, res: Response) => {
+  async (req: Request & { user?: IJWTPayload }, res: Response) => {
     const user = req.user;
-    const result = await DoctorScheduleService.insertIntoDB(user, req.body);
+    const result = await DoctorScheduleService.insertIntoDB(
+      user as IJWTPayload,
+      req.body
+    );
 
     sendResponse(res, {
       statusCode: 201,
--- src/app/modules/doctorSchedule/doctorSchedule.service.ts
+++ src/app/modules/doctorSchedule/doctorSchedule.service.ts
@@ -1,14 +1,27 @@
 import { prisma } from "../../shared/prisma";
+import { IJWTPayload } from "../../types/common";
 
-const insertIntoDB = async (user: any, payload: any) => {
+const insertIntoDB = async (
+  user: IJWTPayload,
+  payload: { scheduleIds: string[] }
+) => {
   console.log({ user, insertIntoDB });
-  const doctorData = prisma.doctor.findUniqueOrThrow({
+  const doctorData = await prisma.doctor.findUniqueOrThrow({
     where: {
       email: user.email,
     },
   });
 
-  return { user, payload };
+  const doctorScheduleData = payload.scheduleIds.map((scheduleId) => ({
+    doctorId: doctorData.id,
+    scheduleId,
+  }));
+
+  console.log(doctorScheduleData);
+
+  return await prisma.doctorSchedules.createMany({
+    data: doctorScheduleData,
+  });
 };
 
 export const DoctorScheduleService = {
