e767de47eb01b6f101659a15c543a43839a89334
raufurislam <raufurislam@gmail.com>
2025-10-21 13:17:09 +0600

    feat(doctor): implement doctor searching functionality

--- src/app/modules/doctor/doctor.controller.ts
+++ src/app/modules/doctor/doctor.controller.ts
@@ -3,10 +3,11 @@ import catchAsync from "../../shared/catchAsync";
 import pick from "../../helper/pick";
 import { DoctorService } from "./doctor.service";
 import sendResponse from "../../shared/sendResponse";
+import { doctorFilterableFields } from "./doctor.constant";
 
 const getAllFromDb = catchAsync(async (req: Request, res: Response) => {
   const options = pick(req.query, ["page", "limit", "sortBy", "sortOrder"]);
-  const filters = pick(req.query, [""]);
+  const filters = pick(req.query, doctorFilterableFields);
 
   const result = await DoctorService.getAllFromDb(filters, options);
 
--- src/app/modules/doctor/doctor.service.ts
+++ src/app/modules/doctor/doctor.service.ts
@@ -1,8 +1,31 @@
+import { Prisma } from "@prisma/client";
 import { IOptions, paginationHelper } from "../../helper/paginationHelper";
+import { doctorSearchableFields } from "./doctor.constant";
+import { object } from "zod";
 
 const getAllFromDb = async (filters: any, options: IOptions) => {
   const { page, limit, skip, sortBy, sortOrder } =
     paginationHelper.calculatePagination(options);
+
+  const { searchTerm, ...filterData } = filters;
+
+  const andConditions: Prisma.DoctorWhereInput[] = [];
+
+  if (searchTerm) {
+    OR: doctorSearchableFields.map((field) => ({
+      contains: searchTerm,
+      mode: "insensitive",
+    }));
+  }
+
+  if (Object.keys(filterData).length > 0) {
+    const filterConditions = Object.keys(filterData).map((key) => ({
+      [key]: {
+        equals: (filterData as any)[key],
+      },
+    }));
+    andConditions.push(...filterConditions);
+  }
 
   console.log({ page, limit, skip, sortBy, sortOrder });
 };
