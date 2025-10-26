595f1e85993414161c793b579eb588f410e9fdc9
raufurislam <raufurislam@gmail.com>
2025-10-27 00:42:04 +0600

    feat(appointment): implement appointment creation api part-2

--- src/app/modules/appointment/appointment.service.ts
+++ src/app/modules/appointment/appointment.service.ts
@@ -1,10 +1,30 @@
+import { prisma } from "../../shared/prisma";
 import { IJWTPayload } from "../../types/common";
 
 const createAppointment = async (
   user: IJWTPayload,
   payload: { doctorId: string; scheduleId: string }
 ) => {
-  console.log(user, payload);
+  const patientData = await prisma.user.findUniqueOrThrow({
+    where: {
+      email: user.email,
+    },
+  });
+
+  const doctorData = await prisma.doctor.findUniqueOrThrow({
+    where: {
+      id: payload.doctorId,
+      isDeleted: false,
+    },
+  });
+
+  const isBookedOrNot = await prisma.doctorSchedules.findFirstOrThrow({
+    where: {
+      doctorId: payload.doctorId,
+      scheduleId: payload.scheduleId,
+      isBooked: false,
+    },
+  });
 };
 
 export const AppointmentService = { createAppointment };
