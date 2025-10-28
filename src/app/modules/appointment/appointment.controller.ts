35080a9e21f5be87cf62e73114734727fb28fe66
raufurislam <raufurislam@gmail.com>
2025-10-28 13:05:55 +0600

    feat(appointment): fetch appointments for doctor and patient dashboards

--- src/app/modules/appointment/appointment.controller.ts
+++ src/app/modules/appointment/appointment.controller.ts
@@ -3,6 +3,7 @@ import catchAsync from "../../shared/catchAsync";
 import { AppointmentService } from "./appointment.service";
 import sendResponse from "../../shared/sendResponse";
 import { IJWTPayload } from "../../types/common";
+import pick from "../../helper/pick";
 
 const createAppointment = catchAsync(
   async (req: Request & { user?: IJWTPayload }, res: Response) => {
@@ -21,6 +22,28 @@ const createAppointment = catchAsync(
   }
 );
 
+const getMyAppointment = catchAsync(
+  async (req: Request & { user?: IJWTPayload }, res: Response) => {
+    const options = pick(req.query, ["page", "limit", "sortBy", "sortOrder"]);
+    const filters = pick(req.query, ["status, paymentStatus"]);
+
+    const user = req.user;
+    const result = await AppointmentService.getMyAppointment(
+      user as IJWTPayload,
+      filters,
+      options
+    );
+
+    sendResponse(res, {
+      statusCode: 201,
+      success: true,
+      message: "Appointment fetched successfully",
+      data: result,
+    });
+  }
+);
+
 export const AppointmentController = {
   createAppointment,
+  getMyAppointment,
 };
--- src/app/modules/appointment/appointment.service.ts
+++ src/app/modules/appointment/appointment.service.ts
@@ -1,3 +1,5 @@
+import { Prisma, UserRole } from "@prisma/client";
+import { IOptions, paginationHelper } from "../../helper/paginationHelper";
 import { stripe } from "../../helper/stripe";
 import { prisma } from "../../shared/prisma";
 import { IJWTPayload } from "../../types/common";
@@ -29,6 +31,13 @@ const createAppointment = async (
   });
 
   const videoCallingId = uuidv4();
+
+  //   console.log({
+  //     patientId: patientData.id,
+  //     doctorData: doctorData.id,
+  //     scheduleId: payload.scheduleId,
+  //     videoCallingId,
+  //   });
 
   const result = await prisma.$transaction(async (tnx) => {
     const appointmentData = await tnx.appointment.create({
@@ -73,18 +82,22 @@ const createAppointment = async (
             product_data: {
               name: `Appointment with ${doctorData.name}`,
             },
-            unit_amount: doctorData.appointmentFee * 100,
+            unit_amount: doctorData.appointmentFee * 100, // in cent
           },
           quantity: 1,
         },
       ],
+
       metadata: {
         appointmentId: appointmentData.id,
         paymentId: paymentData.id,
       },
+
       success_url: `https://www.programming-hero.com/`,
       cancel_url: `https://next.programming-hero.com/`,
     });
+
+    console.log(session);
 
     return { paymentUrl: session.url };
   });
@@ -92,4 +105,67 @@ const createAppointment = async (
   return result;
 };
 
-export const AppointmentService = { createAppointment };
+const getMyAppointment = async (
+  user: IJWTPayload,
+  filters: any,
+  options: IOptions
+) => {
+  const { page, limit, skip, sortBy, sortOrder } =
+    paginationHelper.calculatePagination(options);
+  const { ...filterData } = filters;
+
+  const andConditions: Prisma.AppointmentWhereInput[] = [];
+
+  if (user.role === UserRole.PATIENT) {
+    andConditions.push({
+      patient: {
+        email: user.email,
+      },
+    });
+  } else if (user.role === UserRole.DOCTOR) {
+    andConditions.push({
+      doctor: {
+        email: user.email,
+      },
+    });
+  }
+
+  if (Object.keys(filterData).length > 0) {
+    const filterConditions = Object.keys(filterData).map((key) => ({
+      [key]: {
+        equals: (filterData as any)[key],
+      },
+    }));
+
+    andConditions.push(...filterConditions);
+  }
+
+  const whereConditions: Prisma.AppointmentWhereInput =
+    andConditions.length > 0 ? { AND: andConditions } : {};
+
+  const result = await prisma.appointment.findMany({
+    where: whereConditions,
+    skip,
+    take: limit,
+    orderBy: {
+      [sortBy]: sortOrder,
+    },
+    include:
+      user.role === UserRole.DOCTOR ? { patient: true } : { doctor: true },
+  });
+
+  const total = await prisma.appointment.count({
+    where: whereConditions,
+  });
+
+  return {
+    meta: {
+      total,
+      limit,
+      page,
+    },
+    data: result,
+  };
+};
+
+export const AppointmentService = { createAppointment, getMyAppointment };
