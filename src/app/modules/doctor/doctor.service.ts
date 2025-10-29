c6ff7e504728aa3686828438cfe19831f4f34b94
raufurislam <raufurislam@gmail.com>
2025-10-29 19:22:09 +0600

    feat(review): integrate reviews into doctor and patient data retrieval

--- src/app/modules/doctor/doctor.service.ts
+++ src/app/modules/doctor/doctor.service.ts
@@ -68,6 +68,11 @@ const getAllFromDb = async (filters: any, options: IOptions) => {
           specialties: true,
         },
       },
+      review: {
+        select: {
+          rating: true,
+        },
+      },
     },
   });
 
@@ -220,6 +225,8 @@ const getByIdFromDB = async (id: string): Promise<Doctor | null> => {
           schedule: true,
         },
       },
+
+      review: true,
     },
   });
   return result;
