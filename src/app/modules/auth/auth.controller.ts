d53572ffcda1a76ff1a01af483e0e5f4ce5938cc
Raufur Islam <raufurislam@gmail.com>
2025-11-07 07:08:02 -0800

    Remove getMe function from AuthController
    Removed the getMe function from AuthController.

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -68,16 +68,6 @@ const changePassword = catchAsync(
   }
 );
 
-const getMe = catchAsync(async (req: Request, res: Response) => {
-    const userSession = req.cookies;
-    const result = await AuthService.getMe(userSession);
 
-    sendResponse(res, {
-        statusCode: httpStatus.OK,
-        success: true,
-        message: "User retrive successfully!",
-        data: result,
-    });
-});
 
 export const AuthController = { login, refreshToken, changePassword };
